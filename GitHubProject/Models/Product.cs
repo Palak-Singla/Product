c6a7e2c20d936a02788eb2ab7c54dec6382a2ce8
Palak-Singla <singlapalak34@gmail.com>
2020-04-02 16:17:06 +0530

    third commit

--- GitHubProject/Models/Product.cs
+++ GitHubProject/Models/Product.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace GitHubProject.Models
 {
+    [Table("Product")]
     public class Product
     {
+        [Key]
         public int PId { get; set; }
         public string PName { get; set; }
         public string PDesc { get; set; }
--- GitHubProject/Startup.cs
+++ GitHubProject/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GitHubProject.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,10 @@ namespace GitHubProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ProductDbContext>(options => options.UseSqlServer
+            (Configuration.GetConnectionString("UsersDbConnection")));
         }
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
