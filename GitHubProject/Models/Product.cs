3c9c546e5641393e0349d44e8c2f1739e81c600b
Palak-Singla <singlapalak34@gmail.com>
2020-04-02 15:37:03 +0530

    first commit

---
+++ GitHubProject/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubProject.Models
+{
+    public class Product
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PDesc { get; set; }
+        public int PPrice { get; set; }
+    }
+}
